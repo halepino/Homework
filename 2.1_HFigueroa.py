c95d81b855524a4e577e14fbce0e5377751220c9
halepino <hofigueroa@my365.bellevue.edu>
2021-09-12 21:37:35 -0500

    Update 2.1_HFigueroa.py

--- 2.1_HFigueroa.py
+++ 2.1_HFigueroa.py
@@ -8,33 +8,30 @@ Created on Tue Sep  7 16:23:29 2021
 # Exercise 2.1
 # Week 2 
 
-# INSTRUCTIONS
-# Display the text “Hello World! I wonder why that is always the default coding text to start with”
-# Add two numbers together
-# Subtract a number from another number
-# Multiply two numbers
-# Divide between two numbers
-# Concatenate two strings together (any words)
-# Create a list of 4 items (can be strings, numbers, both)
-# Append an item to your list (again, can be a string, number)
-# Create a tuple with 4 items (can be strings, numbers, both)
+# Hello World
+print("Hello World! I wonder why that is always the default coding text to start with")
 
-print("Hello World!")
+# Operations
 print(2+2)
 print(47-46)
 print(2*35)
 print(40/4)
 
+# Data Types
 string1 = "holy"
+print(string1)
+
 string2 ="buckets"
+print(string2)
+
 string3 = string1 + string2
 print(string3)
 
 list1 = ["blue", "red", "green", "yellow"]
-list1.append("purple")
 print(list1)
 
-tuple1= ("size", "color", "shape","texture", )
-
-
-
+list1.append("purple")
+print(list1
+      )
+tuple1= ("size", "color", "shape","texture")
+print(tuple1)
